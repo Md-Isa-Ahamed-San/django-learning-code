214b8fd131c2aa4b87075c118704e21369ace963
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-09-28 15:08:04 +0600

    complete video 7: Django REST Framework - Dynamic Filtering | Overriding get_queryset() method

--- Starter Code/api/admin.py
+++ Starter Code/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from api.models import Order, OrderItem #registering the models in admin panel
 
 # Register your models here.
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+class OrderAdmin(admin.ModelAdmin):
+    #using inlines so admin use can add order items dynamically
+    inlines=[
+        OrderItemInline
+
+    ]
+admin.site.register(Order, OrderAdmin)
--- Starter Code/api/views.py
+++ Starter Code/api/views.py
@@ -101,6 +101,15 @@ class OrderListAPIView(generics.ListAPIView):
 # [All Products] ---- 1 query
 # = 3 🎯
 
+class UserOrderListAPIView(generics.ListAPIView):
+    # queryset = Order.objects.all()
+    queryset = Order.objects.prefetch_related("items__product")
+    # queryset = Product.objects.filter(stock__gt=0)  # only products in stock. this is for only generic view
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user = self.request.user)
 
 @api_view(['GET'])
 def product_info(request):
